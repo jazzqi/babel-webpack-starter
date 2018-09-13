673afbffc301fad66c9c7b13a986e07e8643e162
qfei <jazzqi@gmail.com>
2018-09-13 12:57:51 +0800

    config workflow

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        // Those babel options also can be put into .bablerc
+        presets: ['@babel/env']
+      }
+    }]
+  }
+}
