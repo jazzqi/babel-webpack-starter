17730a9c612fc63057f914218a0e7d0670e1f564
qfei <jazzqi@gmail.com>
2018-09-13 14:30:03 +0800

    add @babel/polyfill support

---
+++ src/lib.js
@@ -0,0 +1,9 @@
+export const person = {
+  name: 'John',
+  location: 'Appleseed',
+  age: 27
+}
+
+export function sayHello(name) {
+  return `Hello ${name}`;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   devtool: 'source-map',
   entry: {
-    app: './src/app.js'
+    app: ['@babel/polyfill', './src/app.js']
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
